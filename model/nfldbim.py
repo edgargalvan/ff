615bebaaa1cc58a31a18c426ad962dcde89df3c1
edgar <edgar.o.galvan@gmail.com>
2017-12-09 00:20:24 -0800

    add additional parameters to nfldb import

--- model/nfldbim.py
+++ model/nfldbim.py
@@ -32,8 +32,16 @@ def nfldbi(season_year, season_type, weeks):
     home_team = []
     away_team = []
     gamekey = []
-    home_yds = []
-    away_yds = []
+    home_passing_yds = []
+    away_passing_yds = []
+    home_rushing_yds = []
+    away_rushing_yds = []
+    home_rushing_tds = []
+    away_rushing_tds = []
+    home_receiving_tds = []
+    away_receiving_tds = []
+    home_score = []
+    away_score = []
     week = []
     num_games = 0
 
@@ -41,33 +49,51 @@ def nfldbi(season_year, season_type, weeks):
     for i in weeks:
         # find out who plays who
         q = nfldb.Query(db).game(season_year=season_year,
-                                 season_type=season_type, week=i)
+                                 season_type=season_type,
+                                 week=i)
         for g in q.as_games():
             home_team.append(g.home_team)
             away_team.append(g.away_team)
+            home_score.append(g.home_score)
+            away_score.append(g.away_score)
             gamekey.append(g.gamekey)
             week.append(i)
             num_games += 1
 
-    # cycle through each playplayer for yards
+    # cycle through each playplayer for metrics
     for i in range(0, num_games):
         # home team yards
         q = nfldb.Query(db).game(gamekey=gamekey[i], team=home_team[i])
         q.play_player(team=home_team[i])
         pps = q.as_aggregate()
-        home_yds.append(sum(pp.passing_yds for pp in pps))
+        home_passing_yds.append(sum(pp.passing_yds for pp in pps))
+        home_rushing_yds.append(sum(pp.rushing_yds for pp in pps))    
+        home_rushing_tds.append(sum(pp.rushing_tds for pp in pps))
+        home_receiving_tds.append(sum(pp.receiving_tds for pp in pps))
 
         # away team yards
         q = nfldb.Query(db).game(gamekey=gamekey[i], team=away_team[i])
         q.play_player(team=away_team[i])
         pps = q.as_aggregate()
-        away_yds.append(sum(pp.passing_yds for pp in pps))
+        away_passing_yds.append(sum(pp.passing_yds for pp in pps))
+        away_rushing_yds.append(sum(pp.rushing_yds for pp in pps))
+
+        away_rushing_tds.append(sum(pp.rushing_tds for pp in pps))
+        away_receiving_tds.append(sum(pp.receiving_tds for pp in pps))
 
     # save to a new dataframe
     df = pd.DataFrame({'home_team': home_team,
                        'away_team': away_team,
-                       'home_yds': home_yds,
-                       'away_yds': away_yds,
+                       'home_passing_yds': home_passing_yds,
+                       'away_passing_yds': away_passing_yds,
+                       'home_rushing_yds': home_rushing_yds,
+                       'away_rushing_yds': away_rushing_yds,
+                       'home_rushing_tds': home_rushing_tds,
+                       'away_rushing_tds': away_rushing_tds,
+                       'home_receiving_tds': home_receiving_tds,
+                       'away_receiving_tds': away_receiving_tds,
+                       'home_score': home_score,
+                       'away_score': away_score,
                        'week': week,
                        'gamekey': gamekey})
     return df
