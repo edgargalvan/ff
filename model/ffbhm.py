3dab918bb5736eed5d8d4bcefa562a829e705edd
edgar <edgar.o.galvan@gmail.com>
2018-02-14 23:22:36 -0800

    update names

--- model/ffbhm.py
+++ model/ffbhm.py
@@ -59,73 +59,34 @@ def bhm(df, metric='score', K=1, nu=3., sd=2.5):
     return trace
 
 
-def simulate_team_seasons(df, trace, nsims=1000, metric='score',
-                          K=1, burnin=1000):
+def simulate_weeks(df_observed, trace, nsims=1000, metric='score',
+                   K=1, burnin=1000):
     dfs = []
     for i in range(nsims):
-        season = simulate_team_season(df, trace, metric=metric,
-                                      K=K, burnin=burnin)
-        t = create_team_season_table(season, metric)
+        df_week = simulate_games(df_observed, trace, metric=metric,
+                                K=K, burnin=burnin)
+        t = create_table_week(df_week, metric)
         t['iteration'] = i
         dfs.append(t)
     return pd.concat(dfs, ignore_index=True)
 
 
-def simulate_team_season(df, trace, metric='scores', K=1, burnin=10000):
-    # metric for home and away
-    home_metric = 'home_'+metric
-    away_metric = 'away_'+metric
-
-    num_samples = trace['atts'].shape[0]
-    draw = np.random.randint(burnin, num_samples)
-    atts_draw = pd.DataFrame({'att': trace['atts'][draw, :], })
-    defs_draw = pd.DataFrame({'def': trace['defs'][draw, :], })
-    home_draw = trace['home'][draw]
-    intercept_draw = trace['intercept'][draw]
-
-    season = df.copy()
-    season = pd.merge(season, atts_draw, left_on='i_home', right_index=True)
-    season = pd.merge(season, defs_draw, left_on='i_home', right_index=True)
-    season = season.rename(columns={'att': 'att_home', 'def': 'def_home'})
-    season = pd.merge(season, atts_draw, left_on='i_away', right_index=True)
-    season = pd.merge(season, defs_draw, left_on='i_away', right_index=True)
-    season = season.rename(columns={'att': 'att_away', 'def': 'def_away'})
-    season['home'] = home_draw
-    season['intercept'] = intercept_draw
-    season['home_theta'] = season.apply(lambda x:
-                                        np.exp(x['intercept'] +
-                                               x['home'] +
-                                               x['att_home'] +
-                                               x['def_away']), axis=1)
-
-    season['away_theta'] = season.apply(lambda x:
-                                        np.exp(x['intercept'] +
-                                               x['att_away'] +
-                                               x['def_home']), axis=1)
-
-    season[home_metric] = season.apply(lambda x:
-                                       K*np.random.poisson(x['home_theta']),
-                                       axis=1)
-    season[away_metric] = season.apply(lambda x:
-                                       K*np.random.poisson(x['away_theta']),
-                                       axis=1)
-    return season
-
-
-def simulate_seasons(df, trace, teams, nsims=1000, metric='score',
+def simulate_seasons(df_observed, trace, teams, nsims=1000, metric='score',
                      K=1, burnin=1000):
     dfs = []
     for i in range(nsims):
-        season = simulate_season(df, trace, metric, K, burnin)
-        table = create_season_table(season, teams, metric)
+        season = simulate_games(df_observed, trace, metric=metric,
+                                 K=K, burnin=burnin)
+        table = create_table_season(season, teams, metric)
         table['iteration'] = i
         dfs.append(table)
     return pd.concat(dfs, ignore_index=True)
 
 
-def simulate_season(df, trace, metric='score', K=1, burnin=1000):
+def simulate_games(df_observed, trace, metric='score', K=1, burnin=1000):
     """
-    Simulate a season once, using one random draw from the mcmc chain.
+    Simulate a games for a week or season once, using one random draw from the
+    mcmc chain.
     """
     # metric for home and away
     home_metric = 'home_'+metric
@@ -138,58 +99,58 @@ def simulate_season(df, trace, metric='score', K=1, burnin=1000):
     home_draw = trace['home'][draw]
     intercept_draw = trace['intercept'][draw]
 
-    season = df.copy()
-    season = pd.merge(season, atts_draw, left_on='i_home', right_index=True)
-    season = pd.merge(season, defs_draw, left_on='i_home', right_index=True)
-    season = season.rename(columns={'att': 'att_home', 'def': 'def_home'})
-    season = pd.merge(season, atts_draw, left_on='i_away', right_index=True)
-    season = pd.merge(season, defs_draw, left_on='i_away', right_index=True)
-    season = season.rename(columns={'att': 'att_away', 'def': 'def_away'})
-    season['home'] = home_draw
-    season['intercept'] = intercept_draw
-    season['home_theta'] = season.apply(lambda x:
+    df_games = df_observed.copy()
+    df_games = pd.merge(df_games, atts_draw, left_on='i_home', right_index=True)
+    df_games = pd.merge(df_games, defs_draw, left_on='i_home', right_index=True)
+    df_games = df_games.rename(columns={'att': 'att_home', 'def': 'def_home'})
+    df_games = pd.merge(df_games, atts_draw, left_on='i_away', right_index=True)
+    df_games = pd.merge(df_games, defs_draw, left_on='i_away', right_index=True)
+    df_games = df_games.rename(columns={'att': 'att_away', 'def': 'def_away'})
+    df_games['home'] = home_draw
+    df_games['intercept'] = intercept_draw
+    df_games['home_theta'] = df_games.apply(lambda x:
                                         np.exp(x['intercept'] +
                                                x['home'] +
                                                x['att_home'] +
                                                x['def_away']), axis=1)
-    season['away_theta'] = season.apply(lambda x:
+    df_games['away_theta'] = df_games.apply(lambda x:
                                         np.exp(x['intercept'] +
                                                x['att_away'] +
                                                x['def_home']), axis=1)
-    season[home_metric] = season.apply(lambda x:
+    df_games[home_metric] = df_games.apply(lambda x:
                                        K*np.random.poisson(x['home_theta']),
                                        axis=1)
-    season[away_metric] = season.apply(lambda x:
+    df_games[away_metric] = df_games.apply(lambda x:
                                        K*np.random.poisson(x['away_theta']),
                                        axis=1)
-    return season
+    return df_games
 
 
-def create_team_season_table(season, metric):
+def create_table_week(df_week, metric):
     # metric for home and away
     home_metric = 'home_'+metric
     away_metric = 'away_'+metric
 
-    home = season[['home_team', home_metric]].rename(
+    home = df_week[['home_team', home_metric]].rename(
         columns={'home_team': 'team', home_metric: metric})
     # metric for away team
-    away = season[['away_team', away_metric]].rename(
+    away = df_week[['away_team', away_metric]].rename(
         columns={'away_team': 'team', away_metric: metric})
 
     return pd.concat([home, away], axis=0)
 
 
-def create_season_table(season, teams, metric='score'):
+def create_table_season(df_season, teams, metric='score'):
     '''
     use dataframe output of simulate_season to creat summary season table
     '''
     home_metric = 'home_' + metric
     away_metric = 'away_' + metric
 
-    g = season.groupby('i_home')
+    g = df_season.groupby('i_home')
     home = pd.DataFrame({'home_for': g[home_metric].sum(),
                          'home_against': g[away_metric].sum()})
-    g = season.groupby('i_away')
+    g = df_season.groupby('i_away')
     away = pd.DataFrame({'away_for': g[away_metric].sum(),
                          'away_against': g[home_metric].sum()})
     df_table = home.join(away)
@@ -229,9 +190,9 @@ def predictions(df, simuls, teams, nsims=5000, metric='score'):
     simuls.set_index('team', inplace=True)
 
     if season_sim:
-        df_observed = create_season_table(df, teams, metric)
+        df_observed = create_table_season(df, teams, metric)
     else:
-        df_observed = create_team_season_table(df, metric)
+        df_observed = create_table_week(df, metric)
 
     g = simuls.groupby('team')
     hdis = pd.DataFrame({'metric_lower': g[metric].quantile(.1),
@@ -251,12 +212,13 @@ def predictions(df, simuls, teams, nsims=5000, metric='score'):
 def plot_hdis(hdis, metric):
     fig, axs = plt.subplots(figsize=(10, 6))
     axs.scatter(hdis.x, hdis[metric],
-                c=sns.palettes.color_palette()[4], zorder=10,
+                c=sns.palettes.color_palette()[4], zorder = 10,
                 label='Actual  '+metric+'For')
 
     axs.errorbar(hdis.x, hdis.metric_median,
                  yerr=(hdis[['relative_lower', 'relative_upper']].values).T,
-                 fmt='s', c=sns.palettes.color_palette()[5], label='Simulations')
+                 fmt='s', c=sns.palettes.color_palette()[5],
+                 label='Simulations')
 
     axs.set_title('Actual '+metric+' For, and 90% Interval from Simulations, by Team')
     axs.set_xlabel('Team')
@@ -264,5 +226,5 @@ def plot_hdis(hdis, metric):
     axs.set_xlim(0, 20)
     axs.legend()
 
-    _ = axs.set_xticks(hdis.index + .5)
-    _ = axs.set_xticklabels(hdis['team'].values, rotation=45)
+    _= axs.set_xticks(hdis.index + .5)
+    _= axs.set_xticklabels(hdis['team'].values, rotation=45)
